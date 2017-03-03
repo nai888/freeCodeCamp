a1732dce8089d7fe3983ae4c83402d38f2488eaf
Ian A. Cook <nai888@gmail.com>
2017-03-02 22:03:04 -0600

    Pulling JSON from Twitch

--- 06-Twitchtv-App/script.js
+++ 06-Twitchtv-App/script.js
@@ -14,4 +14,36 @@ var colors = {
         dst: "#302000"
     }
 };
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"]
 var data = {};
+
+function getData() {
+    function getURL(type, name) {
+        return "https://wind-bow.gomix.me/twitch-api/" + type + "/" + name + "?callback=?";
+    }
+    $.each(users, function(index, value) {
+        $.getJSON(getURL("streams", value), function(json) {
+            data[value] = json.stream;
+            status = "";
+            if (data[value] !== null) {
+                status = "Online";
+            } else {
+                status = "Offline";
+            }
+
+            function printStatus() {
+                if (status === "Online") {
+                    return "<b>" + status + ":</b> " + data[value]["game"];
+                } else {
+                    return status;
+                }
+            }
+            $("#status-box").append('<div class="stream" id="' + value + '"><h4><a href="https://www.twitch.tv/"' + value + '" target="_blank">' + value + '</a></h4><p>' + printStatus() + '</p></div>');
+        });
+    });
+    console.log(data);
+}
+
+$(document).ready(function() {
+    getData();
+});
