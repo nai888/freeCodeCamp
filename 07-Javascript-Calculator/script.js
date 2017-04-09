3073d120612e491071505b1ffb9d4b29f630681d
Ian A. Cook <nai888@gmail.com>
2017-04-09 18:28:59 -0500

    Building the calculator

--- 07-Javascript-Calculator/script.js
+++ 07-Javascript-Calculator/script.js
@@ -1,16 +1,7 @@
 var colors = {
-    prim: {
-        main: "#00883F",
-        lst: "#38D07F",
-        ler: "#05BE5B",
-        der: "#005E2B",
-        dst: "#002210"
-    },
-    comp: {
-        main: "#C18300",
-        lst: "#FFC345",
-        ler: "#FFAF06",
-        der: "#845A00",
-        dst: "#302000"
-    }
+    acc: "#A7CECB",
+    accDrk: "#8BA6A9",
+    bkg: "#F4EBBE",
+    drk: "#75704E",
+    med: "#CACC90"
 };
