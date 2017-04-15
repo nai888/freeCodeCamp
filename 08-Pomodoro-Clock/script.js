2efac086c01fa06a93cc8a3ab3984bef9b7a7233
Ian A. Cook <nai888@gmail.com>
2017-04-15 16:04:55 -0500

    Starting work on timer

--- 08-Pomodoro-Clock/script.js
+++ 08-Pomodoro-Clock/script.js
@@ -1,1 +1,36 @@
-var time;
+var mins = 25;
+var secs = 0;
+var remMins = mins;
+var remSecs = secs;
+var running = false;
+
+function runTimer() {
+    running = true;
+    var startTime = Date.now();
+    var countDown = setInterval(function () {
+        var now = Date.now();
+        var distance = startTime - now;
+
+    }, 1000);
+}
+
+$(document).ready(function () {
+    var ldgZero;
+    if (secs < 10) {
+        ldgZero = "0";
+    } else {
+        ldgZero = "";
+    }
+    $(".time").text(mins + ":" + ldgZero + secs);
+    $(".tomato").click(function () {
+        if (running) {
+            running = false;
+        } else {
+            runTimer();
+        }
+    });
+    $(".reset").click(function () {
+        remaining = mins;
+        running = false;
+    });
+});
