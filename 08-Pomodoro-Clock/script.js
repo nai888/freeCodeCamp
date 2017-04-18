8adb2c95f271b4369d9cb07570b3bab0aa22c2eb
Ian A. Cook <nai888@gmail.com>
2017-04-18 07:15:34 -0500

    Building out Vue, enabling time mod buttons

--- 08-Pomodoro-Clock/script.js
+++ 08-Pomodoro-Clock/script.js
@@ -3,29 +3,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
         el: "main",
         data: {
             breakMins: 5,
-            breakSecs: 0,
+            breakSecs: "00",
             sessMins: 25,
-            sessSecs: 0,
+            sessSecs: "00",
             timerMins: 25,
-            timerSecs: 0
+            timerSecs: "00"
         },
         computed: {
             breakTime: function () {
-                return this.breakMins + ":" + ldgZero(this.breakSecs) + this.breakSecs;
+                return this.breakMins + ":"/* + ldgZero(this.breakSecs)*/ + this.breakSecs;
             },
             sessTime: function () {
-                return this.sessMins + ":" + ldgZero(this.sessSecs) + this.sessSecs;
+                return this.sessMins + ":"/* + ldgZero(this.sessSecs)*/ + this.sessSecs;
             },
             timerTime: function () {
-                return this.timerMins + ":" + ldgZero(this.timerSecs) + this.timerSecs;
+                return this.timerMins + ":"/* + ldgZero(this.timerSecs)*/ + this.timerSecs;
             }
         },
         methods: {
             minus: function (type) {
-                
+                if (type === 'break' && this.breakMins > 0) {
+                    this.breakMins--;
+                } else if (type === 'session' && this.sessMins > 0) {
+                    this.sessMins--;
+                    if (!running) {
+                        this.timerMins = this.sessMins;
+                    }
+                }
             },
             plus: function (type) {
-
+                if (type === 'break') {
+                    this.breakMins++;
+                } else if (type === 'session') {
+                    this.sessMins++;
+                    if (!running) {
+                        this.timerMins = this.sessMins;
+                    }
+                }
             }
         }
     });
@@ -34,24 +48,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         if (secs < 10) {
             return "0";
         }
-    }
-
-    function displayTime(mins, secs) {
-        return mins + ":" + ldgZero(secs) + secs;
-    }
-
-    function updateBreak() {
-        timer.breakval = displayTime(breakMins, breakSecs);
-    }
-
-    function updateSess() {
-        timer.sessval = displayTime(sessMins, sessSecs);
-    }
-
-    function updateTimer() {
-        timerMins = sessMins;
-        timerSecs = sessSecs;
-        timer.timerTime = displayTime(sessMins, sessSecs);
     }
 
     var running = false;
@@ -65,9 +61,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         }, 1000);
     }
-    updateBreak();
-    updateSess();
-    updateTimer();
+
     document.getElementById("tomato").addEventListener("click", function () {
         if (running) {
             running = false;
@@ -75,35 +69,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
             runTimer();
         }
     });
+
     document.getElementById("reset").addEventListener("click", function () {
         running = false;
         timerMins = sessMins;
         timerSecs = sessSecs;
-    });
-    document.getElementById("break-minus").addEventListener("click", function () {
-        if (breakMins > 0) {
-            breakMins--;
-        }
-        updateBreak();
-    });
-    document.getElementById("break-plus").addEventListener("click", function () {
-        breakMins++;
-        updateBreak();
-    });
-    document.getElementById("session-minus").addEventListener("click", function () {
-        if (sessMins > 0) {
-            sessMins--;
-        }
-        updateSess();
-        if (!running) {
-            updateTimer();
-        }
-    });
-    document.getElementById("session-plus").addEventListener("click", function () {
-        sessMins++;
-        updateSess();
-        if (!running) {
-            updateTimer();
-        }
     });
 });
