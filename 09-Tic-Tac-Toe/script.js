baa1fbf5039860f62811121b3fce343d0b35c7f5
Ian A. Cook <nai888@gmail.com>
2017-04-22 09:59:28 -0500

    Building animations for choices and resetting

--- 09-Tic-Tac-Toe/script.js
+++ 09-Tic-Tac-Toe/script.js
@@ -1,14 +1,45 @@
 var playerX = false;
 var playerO = false;
+var playerToken;
+var cpuToken;
+var scoreX = 0;
+var scoreO = 0;
+var turn = 0;
 
 function choiceMade(choice) {
-    $(".choice").addClass("no-show");
-    $(".game").removeClass("no-show");
+    $(".question, .choices").fadeOut(500, function() {
+        $(".chosen").text("Player " + choice).fadeIn(500, function() {
+            $(".choice").delay(1000).fadeOut(500, function() {
+                $(".game").fadeIn(500);
+                $(".question, .choices").css("display", "block");
+                $(".chosen").css("display","none");
+            });
+        });
+    });
     if (choice === "X") {
         playerX = true;
+        playerToken = "X";
+        cpuToken = "O";
     } else if (choice === "O") {
         playerO = true;
+        playerToken = "O";
+        cpuToken = "X";
     }
+}
+
+function reset() {
+    playerX = false;
+    playerO = false;
+    $(".game").fadeOut(500, function() {
+        $("td").text("");
+        $(".choice").fadeIn(500);
+    });
+}
+
+function resetScore() {
+    scoreX = 0;
+    scoreO = 0;
+    reset();
 }
 
 $(document).ready(function() {
@@ -17,5 +48,8 @@ $(document).ready(function() {
     });
     $("span.O").click(function() {
         choiceMade("O");
+    });
+    $(".reset").click(function() {
+        resetScore();
     });
 });
