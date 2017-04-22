38a17ecfaea095dc213acadd666e4f8888b66030
Ian A. Cook <nai888@gmail.com>
2017-04-22 15:00:49 -0500

    Developing player turn and cpu turn functions

--- 09-Tic-Tac-Toe/script.js
+++ 09-Tic-Tac-Toe/script.js
@@ -1,30 +1,28 @@
 function ticTacToe() {
     var playerX = false;
     var playerO = false;
-    var playerToken;
-    var cpuToken;
-    var scoreX = 0;
-    var scoreO = 0;
+    var playerToken = "";
+    var cpuToken = "";
+    var playerScore = 0;
+    var cpuScore = 0;
     var turn = 0;
+    var isPlayerTurn = false;
 
     $(".choiceX").click(function () {
+        console.log('choiceMade("X")');
         choiceMade("X");
+        console.log('playerTurn()')
+        playerTurn();
     });
 
     $(".choiceO").click(function () {
+        console.log('choiceMade("O")');
         choiceMade("O");
+        console.log('cpuTurn()')
+        setTimeout(cpuTurn, 4000);
     });
 
     function choiceMade(choice) {
-        $(".question, .choices").fadeOut(500, function () {
-            $(".chosen").text("Player " + choice).fadeIn(500, function () {
-                $(".choice").delay(1000).fadeOut(500, function () {
-                    $(".game").fadeIn(500);
-                    $(".question, .choices").css("display", "block");
-                    $(".chosen").css("display", "none");
-                });
-            });
-        });
         if (choice === "X") {
             playerX = true;
             playerToken = "X";
@@ -34,63 +32,194 @@ function ticTacToe() {
             playerToken = "O";
             cpuToken = "X";
         }
-    }
-
-    function reset() {
-        playerX = false;
-        playerO = false;
-        $(".game").fadeOut(500, function () {
-            $("td").text("");
-            $(".winner").text("");
-            $(".choice").fadeIn(500);
+        $(".question, .choices").fadeOut(500, function () {
+            $(".chosen").text("Player " + choice).fadeIn(500, function () {
+                $(".choice").delay(1000).fadeOut(500, function () {
+                    $(".game, .score, .gamegrid").fadeIn(500, function () {
+                        $(".question, .choices").css("display", "block");
+                        $(".chosen").css("display", "none");
+                    });
+                });
+            });
         });
     }
 
-    $(".reset").click(function () {
-        resetScore();
-    });
+    function nextTurn() {
+        console.log('Turn: ' + turn);
+        if ((turn % 2 === 0 && playerX) || (turn % 2 !== 0 && playerO)) {
+            console.log('playerTurn()');
+            playerTurn();
+        } else {
+            setTimeout(function () {
+                console.log('cpuTurn()');
+                cpuTurn();
+            }, 1000);
+        }
+    }
 
-    function resetScore() {
-        scoreX = 0;
-        scoreO = 0;
-        updateScore();
-        reset();
+    function playerTurn() {
+        isPlayerTurn = true;
+
+        function nextStep() {
+            isPlayerTurn = false;
+            turn++;
+            if (turn >= 5) {
+                console.log('checkForWinner()');
+                checkForWinner();
+            } else {
+                console.log('nextTurn()');
+                nextTurn();
+            }
+        }
+
+        $("button.one").click(function () {
+            if ($("button.one").text() === "" && isPlayerTurn) {
+                $("button.one").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.two").click(function () {
+            if ($("button.two").text() === "" && isPlayerTurn) {
+                $("button.two").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.three").click(function () {
+            if ($("button.three").text() === "" && isPlayerTurn) {
+                $("button.three").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.four").click(function () {
+            if ($("button.four").text() === "" && isPlayerTurn) {
+                $("button.four").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.five").click(function () {
+            if ($("button.five").text() === "" && isPlayerTurn) {
+                $("button.five").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.six").click(function () {
+            if ($("button.six").text() === "" && isPlayerTurn) {
+                $("button.six").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.seven").click(function () {
+            if ($("button.seven").text() === "" && isPlayerTurn) {
+                $("button.seven").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.eight").click(function () {
+            if ($("button.eight").text() === "" && isPlayerTurn) {
+                $("button.eight").text(playerToken);
+                nextStep();
+            }
+        });
+        $("button.nine").click(function () {
+            if ($("button.nine").text() === "" && isPlayerTurn) {
+                $("button.nine").text(playerToken);
+                nextStep();
+            }
+        });
+    }
+
+    function cpuTurn() {
+        var tileChoice;
+        if (turn === 0) {
+            console.log('random tile choice');
+            tileChoice = Math.floor(Math.random() * 9) + 1;
+            console.log(tileChoice);
+        } else {
+            console.log('strategic tile choice');
+        }
+        switch (tileChoice) {
+            case 1:
+                $("button.one").text(cpuToken);
+                break;
+            case 2:
+                $("button.two").text(cpuToken);
+                break;
+            case 3:
+                $("button.three").text(cpuToken);
+                break;
+            case 4:
+                $("button.four").text(cpuToken);
+                break;
+            case 5:
+                $("button.five").text(cpuToken);
+                break;
+            case 6:
+                $("button.six").text(cpuToken);
+                break;
+            case 7:
+                $("button.seven").text(cpuToken);
+                break;
+            case 8:
+                $("button.eight").text(cpuToken);
+                break;
+            case 9:
+                $("button.nine").text(cpuToken);
+                break;
+        }
+        turn++;
+        console.log('Now turn: ' + turn);
+        if (turn >= 5) {
+            console.log('checkForWinner()');
+            checkForWinner();
+        } else {
+            console.log('nextTurn()');
+            nextTurn();
+        }
     }
 
     function checkForWinner() {
         var tie = false;
+
         function checkSolutions(player) {
-            if ($(".one").text() === player && $(".two").text() === player && $(".three").text() === player) {
+            if ($("button.one").text() === player && $("button.two").text() === player && $("button.three").text() === player) {
                 return true;
-            } else if ($(".four").text() === player && $(".five").text() === player && $(".six").text() === player) {
+            } else if ($("button.four").text() === player && $("button.five").text() === player && $("button.six").text() === player) {
                 return true;
-            } else if ($(".seven").text() === player && $(".eight").text() === player && $(".nine").text() === player) {
+            } else if ($("button.seven").text() === player && $("button.eight").text() === player && $("button.nine").text() === player) {
                 return true;
-            } else if ($(".one").text() === player && $(".four").text() === player && $(".seven").text() === player) {
+            } else if ($("button.one").text() === player && $("button.four").text() === player && $("button.seven").text() === player) {
                 return true;
-            } else if ($(".two").text() === player && $(".five").text() === player && $(".eight").text() === player) {
+            } else if ($("button.two").text() === player && $("button.five").text() === player && $("button.eight").text() === player) {
                 return true;
-            } else if ($(".three").text() === player && $(".six").text() === player && $(".nine").text() === player) {
+            } else if ($("button.three").text() === player && $("button.six").text() === player && $("button.nine").text() === player) {
                 return true;
-            } else if ($(".one").text() === player && $(".five").text() === player && $(".nine").text() === player) {
+            } else if ($("button.one").text() === player && $("button.five").text() === player && $("button.nine").text() === player) {
                 return true;
-            } else if ($(".three").text() === player && $(".five").text() === player && $(".seven").text() === player) {
+            } else if ($("button.three").text() === player && $("button.five").text() === player && $("button.seven").text() === player) {
                 return true;
             } else {
                 return false;
             }
         }
-        if (checkSolutions("X")) {
-            scoreX += 1;
-            showWinner("X");
-        } else if (checkSolutions("O")) {
-            scoreO += 1;
-            showWinner("O");
-        } else if (turn = 9) {
+        if (checkSolutions(playerToken)) {
+            console.log('player win');
+            playerScore += 1;
+            console.log('showWinner(playerToken)');
+            showWinner(playerToken);
+        } else if (checkSolutions(cpuToken)) {
+            console.log('cpu win');
+            cpuScore += 1;
+            console.log('showWinner()cpuToken)');
+            showWinner(cpuToken);
+        } else if (turn === 9) {
+            console.log('tie');
             tie = true;
+            console.log('showWinner("tie")');
             showWinner("tie");
+        } else {
+            console.log('nextTurn()');
+            nextTurn();
         }
-        updateScore();
     }
 
     function showWinner(winner) {
@@ -98,17 +227,45 @@ function ticTacToe() {
             if (winner === "tie") {
                 $(".winner").text("It was a tie.");
             } else {
-                $(".winner").text("Player " + winner + "wins!");
+                $(".winner").text("Player " + winner + " wins!");
             }
             $(".winner").fadeIn(500).delay(1000).fadeOut(500, function () {
+                console.log('reset()');
                 reset();
             });
         });
+        console.log('updateScore()');
+        updateScore();
     }
 
     function updateScore() {
-        $(".scoreX").text(scoreX);
-        $(".scoreO").text(scoreO);
+        $(".player-score").text(playerScore);
+        $(".cpu-score").text(cpuScore);
+    }
+
+    function reset() {
+        playerX = false;
+        playerO = false;
+        turn = 0;
+        $(".game, .score").fadeOut(500, function () {
+            $("button").text("");
+            $(".winner").text("");
+            $(".choice").fadeIn(500);
+        });
+    }
+
+    $(".reset").click(function () {
+        console.log('resetScore()');
+        resetScore();
+    });
+
+    function resetScore() {
+        scoreX = 0;
+        scoreO = 0;
+        console.log('updateScore()');
+        updateScore();
+        console.log('reset()');
+        reset();
     }
 }
 
