4bfef6b5f88b2a550fcb471f76690ed9099f6a57
Ian A. Cook <nai888@gmail.com>
2017-05-05 21:04:46 -0500

    Add sounds, timer, and win scenario

--- 10-Simon-Game/script.js
+++ 10-Simon-Game/script.js
@@ -4,7 +4,12 @@ var power = false;
 var playing = false;
 var strict = false;
 var lightsInterval;
+var playerTimeout;
 var digits;
+const ulSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+const urSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+const llSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+const lrSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 
 function translateToBtns(btn) {
 	switch (btn) {
@@ -33,8 +38,6 @@ function play() {
 			digits = pattern.length;
 		}
 		$(".digits").text(digits);
-		console.log("pattern:");
-		console.log(pattern);
 		lightPattern();
 	}
 }
@@ -43,24 +46,37 @@ function lightPattern() {
 	if (power && playing) {
 		var speed;
 		if (pattern.length < 5) {
-			speed = 1800;
+			speed = 1600;
 		} else if (pattern.length < 9) {
-			speed = 1500;
+			speed = 1300;
 		} else if (pattern.length < 13) {
-			speed = 1200;
+			speed = 1000;
 		} else {
-			speed = 900;
+			speed = 700;
 		}
 		var t = 0;
 		lightsInterval = setInterval(function () {
 			var btn = translateToBtns(pattern[t]);
 			$("." + btn).addClass("on");
+			playAud(parseInt(pattern[t]));
 			setTimeout(function () {
 				$("." + btn).removeClass("on");
 			}, (speed / 2));
 			t++;
 			if (t >= pattern.length) {
 				clearInterval(lightsInterval);
+				var playTimer = 5000;
+				if (pattern.length > 6) {
+					playTimer = (pattern.length * 1000) * 0.8;
+				}
+				playerTimeout = setTimeout(function () {
+					if (strict) {
+						strictWrong();
+					} else {
+						wrong();
+					}
+					rePattern.length = 0;
+				}, playTimer);
 				clickable();
 			}
 		}, speed);
@@ -69,13 +85,11 @@ function lightPattern() {
 
 function wrong() {
 	unclickable();
-	$(".digits").text("!-");
-	console.log("screen: !-");
+	$(".digits").text("-!");
 	setTimeout(function () {
 		$(".digits").text(digits);
-		console.log("screen: " + digits + ", lightPattern()");
 		lightPattern();
-	}, 1000);
+	}, 2000);
 }
 
 function strictWrong() {
@@ -84,29 +98,69 @@ function strictWrong() {
 	$(".digits").text("!!");
 	setTimeout(function () {
 		play();
-	}, 1000);
+	}, 2000);
+}
+
+function win() {
+	unclickable();
+	pattern = [];
+	rePattern = [];
+	$(".digits").text("$$");
+	var on = true;
+	$(".btn").addClass("on");
+	var time = 4000;
+	var flash = setInterval(function () {
+		if (on) {
+			on = false;
+			$(".btn").removeClass("on");
+		} else {
+			on = true;
+			$(".btn").addClass("on");
+		}
+	}, time / 8);
+	setTimeout(function () {
+		clearInterval(flash);
+		$(".btn").removeClass("on");
+		play();
+	}, time);
 }
 
 function clickable() {
-	$(".ul, .ur, .ll, .lr").removeClass("unclickable").addClass("clickable");
+	$(".btn").removeClass("unclickable").addClass("clickable");
 }
 
 function unclickable() {
-	$(".ul, .ur, .ll, .lr").removeClass("clickable").addClass("unclickable");
+	$(".btn").removeClass("clickable").addClass("unclickable");
+}
+
+function playAud(btn) {
+	switch (btn) {
+		case 0:
+			ulSound.play();
+			break;
+		case 1:
+			urSound.play();
+			break;
+		case 2:
+			llSound.play();
+			break;
+		case 3:
+			lrSound.play();
+			break;
+	}
 }
 
 $(document).ready(function () {
-	var $ul = $(".ul");
-	var $ur = $(".ur");
-	var $ll = $(".ll");
-	var $lr = $(".lr");
 	$(".power").click(function () {
 		if (power) {
 			power = false;
 			playing = false;
 			strict = false;
 			clearInterval(lightsInterval);
+			clearTimeout(playerTimeout);
 			pattern = [];
+			rePattern = [];
+			unclickable();
 			$(".power, .start, .strict, .screen").removeClass("on");
 		} else {
 			power = true;
@@ -118,7 +172,10 @@ $(document).ready(function () {
 			if (playing) {
 				playing = false;
 				clearInterval(lightsInterval);
+				clearTimeout(playerTimeout);
 				pattern = [];
+				rePattern = [];
+				unclickable();
 				$(".start").removeClass("on");
 			} else {
 				playing = true;
@@ -138,27 +195,34 @@ $(document).ready(function () {
 			}
 		}
 	});
+	$(".btn").mousedown(function () {
+		$(this).addClass("on");
+		playAud(parseInt($(this).attr("data-val"), 10));
+
+	})
+	$(".btn").mouseup(function () {
+		$(this).removeClass("on");
+	})
 	$(".btn").click(function () {
 		rePattern.push(parseInt($(this).attr("data-val"), 10));
-		console.log("rePattern:");
-		console.log(rePattern);
 		if (rePattern.length >= pattern.length) {
+			clearTimeout(playerTimeout);
 			var equal = true;
 			for (let l = 0; l < rePattern.length; l++) {
 				if (rePattern[l] !== pattern[l]) {
 					equal = false;
 				}
 			}
-			console.log(equal);
 			if (equal) {
-				console.log("ready to move on: play()");
-				play();
+				if (pattern.length === 20) {
+					win();
+				} else {
+					play();
+				}
 			} else {
 				if (strict) {
-					console.log("wrong: strictWrong()");
 					strictWrong();
 				} else {
-					console.log("wrong: wrong()");
 					wrong();
 				}
 			}
