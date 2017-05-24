36b3431ac42e975108b1586c63a21964f1b32310
Ian A. Cook <nai888@gmail.com>
2017-05-23 21:33:15 -0500

    Setting up state and figuring out local storage

--- 13-Recipe-Box/src/App.js
+++ 13-Recipe-Box/src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    const defaultData = [{ name: "Pumpkin Pie", ingredients: ["Sweetened condensed milk", "Eggs", "Pumpkin pie spice", "Pie crust"] }, { name: "Spaghetti", ingredients: ["Pasta", "Sauce", "Vegetables"] }];
+    if (typeof (Storage) !== "undefined" && localStorage.recipeData === undefined) {
+      // Re-enable this when ready to work with localStorage
+      // localStorage.setItem("recipeData", defaultData);
+    }
+    this.state = { data: defaultData };
+  }
+
+  handleClick(whichData) {
+    if (typeof (Storage) !== "undefined") {
+      localStorage.setItem("recipeData", whichData);
+    }
+    this.setState({ whichData });
+  }
+
   render() {
     return (
       <div className="App">
