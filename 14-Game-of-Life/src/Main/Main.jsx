9683bf9fe30d6bbc44d1a06768e2b396abe1d3ab
Ian A. Cook <nai888@gmail.com>
2017-06-17 12:08:30 -0500

    Added controls and counter, working on status checker

--- 14-Game-of-Life/src/Main/Main.jsx
+++ 14-Game-of-Life/src/Main/Main.jsx
@@ -2,25 +2,56 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Main.css';
 
+function Button(props) {
+  var text = "";
+  if (props.class === "start-stop") {
+    text = props.playing ? "Stop" : "Start";
+  } else if (props.class === "randomize") {
+    text = "Randomize";
+  } else if (props.class === "clear") {
+    text = "Clear";
+  }
+
+  return (
+    <button
+      className={props.class}
+      onClick={props.onClick}
+    >{text}</button>
+  );
+}
+
+Button.propTypes = {
+  playing: PropTypes.bool,
+  class: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
 function Cell(props) {
-  return <div className={"cell " + props.status + " cell" + props.index} />
+  return (
+    <div
+      className={"cell " + props.status}
+      data-index={props.index}
+      onClick={props.onClick}
+    />
+  );
 }
 
 Cell.propTypes = {
   status: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired
+  index: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
 }
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.cellBirth = this.cellBirth.bind(this);
-    this.cellLive = this.cellLive.bind(this);
-    this.cellDead = this.cellDead.bind(this);
+    this.newBoard = this.newBoard.bind(this);
     this.randomize = this.randomize.bind(this);
+    this.cellAge = this.cellAge.bind(this);
+    this.runGame = this.runGame.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleStartStop = this.handleStartStop.bind(this);
-    this.state = { playing: false, board: [] };
+    this.state = { playing: false, board: [], generation: 0 };
   }
 
   componentDidMount() {
@@ -28,33 +59,120 @@ class Main extends Component {
     for (var i = 0; i < 3200; i++) {
       newBoard.push("dead");
     }
-    newBoard[248] = "birth";
-    newBoard[252] = "live";
     this.setState({ board: newBoard });
   }
 
-  cellBirth(cell) {
-    // change the cell's status in the state to "birth"
-  }
-
-  cellLive(cell) {
-    // if the cell's status in the state is "birth" and it continues to live, change the cell's status in the state to "live"
-  }
-
-  cellDead(cell) {
-    // change the cell's status in the state to "dead"
+  newBoard() {
+    var newBoard = this.state.board;
+    for (var i = 0; i < newBoard.length; i++) {
+      newBoard[i] = "dead";
+    }
+    this.setState({ board: newBoard });
   }
 
   randomize() {
-    // if running, pause the game; clear the board and randomly fill it again
+    this.handleStartStop();
+    var newBoard = this.state.board;
+    for (var i = 0; i < newBoard.length; i++) {
+      const j = Math.floor(Math.random() * 5);
+      newBoard[i] = j === 0 ? "live" : "dead";
+    }
+    this.setState({ board: newBoard });
+  }
+
+  cellAge(cell) {
+    var newBoard = this.state.board;
+    const cells = this.state.board.length; // The total number of cells
+    const width = 80; // The width of cells on the board
+    for (var i = 0; i < cells; i++) {
+      // Identifies the location of the cell on the physical board
+      function isNorth(c) { return c < width ? true : false; }
+      function isSouth(c) { return c >= cells - width ? true : false; }
+      function isWest(c) { return c % width === 0 ? true : false; }
+      function isEast(c) { return (c + 1) % width === 0 ? true : false; }
+      function isNonEdge(c) { return (!isNorth(c) && !isSouth(c) && !isWest(c) && !isEast(c)) ? true : false; }
+
+      var count = 0;
+
+      // Check northwest cell
+      if (isNorth(i) && !isWest(i)) { // Northern edge, not the northwest corner
+        if (this.state.board[i + cells - width - 1] === "live" || this.state.board[i + cells - width - 1] === "birth") {
+          count++;
+        }
+      } else if (isWest(i) && !isNorth(i)) { // Western edge, not the northwest corner
+        if (this.state.board[i - 1] === "live" || this.state.board[i - 1] === "birth") {
+          count++;
+        }
+      } else if (isNorth(i) && isWest(i)) { // Northwest corner
+        if (this.state.board[i + cells - 1] === "live" || this.state.board[i + cells - 1] === "birth") {
+          count++;
+        }
+      } else { // Remainder of board handled normally
+        if (this.state.board[i - width - 1] === "live" || this.state.board[i - width - 1] === "birth") {
+          count++;
+        }
+      }
+      // Check north cell
+      if (!isNorth(i) && (this.state.board[i - width] === "live" || this.state.board[i - width] === "birth")) {
+        count++
+      }
+      // Check northeast cell
+      if ((!isNorth(i) && !isEast(i)) && (this.state.board[i - width + 1] === "live" || this.state.board[i - width + 1] === "birth")) {
+        count++
+      }
+      // Check west cell
+      if (!isWest(i) && (this.state.board[i - 1] === "live" || this.state.board[i - 1] === "birth")) {
+        count++
+      }
+      // Check east cell
+      if (!isEast(i) && (this.state.board[i + 1] === "live" || this.state.board[i + 1] === "birth")) {
+        count++
+      }
+      // Check southwest cell
+      if ((!isSouth(i) && !isWest(i)) && (this.state.board[i + width - 1] === "live" || this.state.board[i + width - 1] === "birth")) {
+        count++
+      }
+      // Check south cell
+      if (!isSouth(i) && (this.state.board[i + width] === "live" || this.state.board[i + width] === "birth")) {
+        count++
+      }
+      // Check southeast cell
+      if ((!isSouth(i) && !isEast(i)) && (this.state.board[i + width + 1] === "live" || this.state.board[i + width + 1] === "birth")) {
+        count++
+      }
+
+      // Rules of the game
+      if ((this.state.board[i] === "live" || this.state.board[i] === "birth") && (count < 2 || count > 3)) {
+        newBoard[i] = "dead";
+      } else if (this.state.board[i] === "dead" && count === 3) {
+        newBoard[i] = "birth";
+      } else if (this.state.board[i] === "birth" && (count === 2 || count === 3)) {
+        newBoard[i] = "live";
+      }
+    }
+    this.state.board === newBoard ? this.handleStartStop() : this.setState({ board: newBoard });
+  }
+
+  runGame() {
+    /* Disabled for now
+    if (this.state.playing) {
+      const newGeneration = this.state.generation + 1;
+      this.cellAge();
+      this.setState({ generation: newGeneration });
+      setTimeout(() => { this.runGame(); }, 1000);
+    }
+    */
   }
 
   handleClick(e) {
-    // if clicked cell's status is "dead", change to "live"; if clicked cell's status is "birth" or "live", change to "dead"
+    const i = e.target.dataset.index;
+    var newBoard = this.state.board;
+    newBoard[i] = newBoard[i] === "dead" ? "live" : "dead";
+    this.setState({ board: newBoard });
   }
 
   handleStartStop() {
-    // if paused, start the game; if running, pause the game
+    this.state.playing ? this.setState({ playing: false }, this.runGame()) : this.setState({ playing: true }, this.runGame());
   }
 
   render() {
@@ -63,6 +181,7 @@ class Main extends Component {
         status={status}
         index={i}
         key={i}
+        onClick={this.handleClick}
       />
     );
 
@@ -72,6 +191,24 @@ class Main extends Component {
           {cells}
         </div>
         <div className="controls">
+          <div className="buttons">  
+            <Button
+              playing={this.state.playing}
+              class="start-stop"
+              onClick={this.handleStartStop}
+            />
+            <Button
+              class="randomize"
+              onClick={this.randomize}
+            />
+            <Button
+              class="clear"
+              onClick={this.newBoard}
+            />
+          </div>
+          <div className="generations">
+            <h2>Generations: {this.state.generation}</h2>
+          </div>
         </div>
       </main>
     );
