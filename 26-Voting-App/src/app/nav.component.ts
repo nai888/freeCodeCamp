eb444052003416a1461642ac44dcf0657cc21698
Ian A. Cook <nai888@gmail.com>
2017-07-30 12:25:59 -0500

    Updating nav

--- 26-Voting-App/src/app/nav.component.ts
+++ 26-Voting-App/src/app/nav.component.ts
@@ -1,20 +1,48 @@
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute, ParamMap } from '@angular/router'
+import { Component, OnDestroy } from '@angular/core'
+import { ActivatedRoute, Router, ParamMap } from '@angular/router'
 import { Location } from '@angular/common'
+import { Subscription } from 'rxjs/Subscription'
 
-import { loggedIn } from './authentication.service'
+import { AuthService } from './auth.service'
 
 @Component({
   selector: 'nav-comp',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+export class NavComponent implements OnDestroy {
+  private _loggedInSubsc: Subscription
+  loggedIn: boolean
+  private _idSubsc: Subscription
+  id: string
+  private _usernameSubsc: Subscription
+  username: string
+  private _displayNameSubsc: Subscription
+  displayName: string
 
-export class NavComponent {
   constructor(
-    private route: ActivatedRoute,
-    private location: Location
-  ) { }
-  loggedIn = loggedIn
-  name = "Ian A. Cook"
+    private _authService: AuthService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _location: Location
+  ) {
+    this._loggedInSubsc = this._authService.isLoggedIn().subscribe(loggedIn => this.loggedIn = loggedIn)
+    this._idSubsc = this._authService.getId().subscribe(id => this.id = id)
+    this._usernameSubsc = this._authService.getUsername().subscribe(username => this.username = username)
+    this._displayNameSubsc = this._authService.getDisplayName().subscribe(displayName => this.displayName = displayName)
+  }
+
+  logOut: () => void = () => {
+    if (this.loggedIn) {
+      this._authService.logOut()
+      this._router.navigate([''])
+    }
+  }
+
+  ngOnDestroy() {
+    this._loggedInSubsc.unsubscribe()
+    this._idSubsc.unsubscribe()
+    this._usernameSubsc.unsubscribe()
+    this._displayNameSubsc.unsubscribe()
+  }
 }
