f52ddc823a8db5dfe0661e0e630eb79cb6bd48b5
Ian A. Cook <nai888@gmail.com>
2017-08-06 10:25:21 -0500

    Reroute on guarded route

--- 26-Voting-App/src/app/routes.guard.ts
+++ 26-Voting-App/src/app/routes.guard.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@angular/core'
-import { CanActivate } from '@angular/router'
+import { Router, CanActivate } from '@angular/router'
 
 import { AuthService } from './auth.service'
 
 @Injectable()
 export class LoggedInGuard implements CanActivate {
-  constructor(private _authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
   canActivate(): boolean {
     // can only activate this route if logged in
-    return this._authService.isLoggedIn().getValue()
+    if (this.authService.isLoggedIn().getValue()) {
+      return true
+    } else {
+      this.router.navigate(['/login'])
+      return false
+    }
   }
 }
 
 @Injectable()
 export class LoggedOutGuard implements CanActivate {
-  constructor(private _authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
   canActivate(): boolean {
     // can only activate this route if logged out
-    return !this._authService.isLoggedIn().getValue()
+    if (this.authService.isLoggedIn().getValue()) {
+      this.router.navigate(['/'])
+      return false
+    } else {
+      return true
+    }
   }
 }
