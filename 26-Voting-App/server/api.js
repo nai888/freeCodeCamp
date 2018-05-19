9992b5db818b6e43aba9c8e33b1dd044e1215c03
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2018-05-19 08:40:43 -0500

    Re-add server and api files

--- 26-Voting-App/server/api.js
+++ 26-Voting-App/server/api.js
@@ -2,3 +2,134 @@
 
 var cors = require('cors')
 var https = require('https')
+var GraphQLClient = require('graphql-request').GraphQLClient
+
+module.exports = function (app, db, collection) {
+  var blankUser = {
+    loggedin: false,
+    login: undefined,
+    name: undefined
+  }
+  var user = blankUser
+  var token
+  var clientID = process.env.gitHubID
+  var clientSecret = process.env.gitHubSecret
+  var appUrl = process.env.appUrl
+  var redir
+
+  app.get('/auth/github', function (req, res) {
+    redir = req.query.url
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientID}`)
+  })
+
+  app.get('/auth/github/callback', function (req, res) {
+    var code = req.query.code
+
+    var options = {
+      protocol: 'https:',
+      hostname: 'github.com',
+      method: 'POST',
+      path: '/login/oauth/access_token?client_id=' + clientID + '&client_secret=' + clientSecret + '&code=' + code,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+
+    var newReq = https.request(options, function (resp) {
+      var data
+
+      resp.on('data', function (chunk) {
+        data = JSON.parse(chunk.toString('utf8'))
+      })
+
+      resp.on('error', function (e) {
+        console.error(e.message || e.error || e)
+      })
+
+      resp.on('end', function () {
+        token = data.access_token
+        getUserData(token)
+      })
+    })
+    newReq.end()
+
+    function getUserData (token) {
+      var client = new GraphQLClient('https://api.github.com/graphql', {
+        headers: {
+          'Authorization': 'token ' + token,
+          'User-Agent': 'nai888'
+        }
+      })
+
+      var query = '{\nviewer {\nlogin\nname\n}\n}'
+
+      client.request(query).then(function (data) {
+        user.login = data.viewer.login
+        user.name = data.viewer.name
+
+        if (user.login && user.name) {
+          user.loggedin = true
+        }
+
+        res.redirect(`${appUrl}/loggedin/${user.login}/${user.name}?redir=${redir}`)
+      })
+    }
+  })
+
+  app.get('/api/logout', function (req, res) {
+    user = blankUser
+    var options = {
+      protocol: 'https:',
+      hostname: 'github.com',
+      method: 'DELETE',
+      path: '/applications/' + clientID + '/tokens/' + token,
+      headers: {
+        'Authorization': 'token ' + token,
+        'User-Agent': 'nai888'
+      }
+    }
+    https.request(options)
+  })
+
+  var corsOptions = {
+    origin: appUrl
+  }
+
+  var polls = db.collection(collection)
+
+  app.get('/api/polls', cors(corsOptions), function (req, res) {
+    var name = req.query.name
+
+    if (name) {
+      polls.find({ 'owner': name }, { '_id': 0 }).sort({ '_id': 1 }).toArray(function (err, docs) {
+        if (err) {
+          console.error(err)
+        } else {
+          res.json(docs)
+        }
+      })
+    } else {
+      polls.count().then(function (num) {
+        res.send(num.toString())
+      })
+    }
+  })
+
+  app.options('/api/poll', cors(corsOptions))
+
+  app.get('/api/poll', cors(corsOptions), function (req, res) {
+    var id = +req.query.id
+
+    polls.findOne({ 'id': id }).then(function (poll) {
+      res.json(poll)
+    })
+  })
+
+  app.delete('/api/poll', cors(corsOptions), function (req, res) {
+    var id = +req.query.id
+
+    polls.deleteOne({ 'id': id }).then(function (result) {
+      res.send(result)
+    })
+  })
+}
--- 26-Voting-App/server/server.js
+++ 26-Voting-App/server/server.js
@@ -10,3 +10,49 @@ var flash = require('connect-flash')
 var session = require('express-session')
 
 var api = require('./api')
+
+// Create app
+var app = express()
+app.use(session({
+  secret: 'fcc-voting-app',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 60 * 1000 }
+}))
+app.use(flash())
+
+// Create server
+var server = http.createServer(app)
+
+// Set port
+var port = normalizePort(process.env.svrPORT || '3000')
+app.set('port', port)
+
+// Log listener
+function onListening () {
+  var addr = server.address()
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port
+  console.log('Listening on ' + bind)
+}
+
+// Start server
+server.listen(app.get('port'))
+server.on('listening', onListening)
+
+// Serve homepage
+app.use('', express.static(path.join(process.cwd()))) // May need to be path.join(process.cwd(), 'public')
+
+// Connect database
+var dbURI = 'mongodb://' + process.env.dbuser + ':' + process.env.dbpassword + '@' + process.env.dburl + ':' + process.env.dbport + '/' + process.env.dbname
+MongoClient.connect(dbURI, function (err, client) {
+  if (err) {
+    console.error(err)
+  } else {
+    var db = client.db(process.env.dbname)
+    var collection = process.env.collection
+    db.createCollection(collection)
+    api(app, db, collection)
+  }
+})
