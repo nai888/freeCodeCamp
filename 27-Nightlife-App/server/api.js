9fca000f9f18a10b06e1a0cb8a2e67df85160588
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2018-06-01 18:14:41 -0500

    Build API for fetching Yelp data
    Build server/api.js file. Build https request to retrieve Yelp
    businesses. Delete dummy data from App-Main and add fetch method to call
    the /api request.

---
+++ 27-Nightlife-App/server/api.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var https = require('https')
+var yelpApiKey = process.env.YELP_API_KEY
+
+module.exports = function (app, db, collection) {
+  var user
+  var bars = db.collection(collection)
+
+  app.get('/api', function (req, res) {
+    var loc = req.query.loc
+
+    var options = {
+      protocol: 'https:',
+      hostname: 'api.yelp.com',
+      path: `/v3/businesses/search?location=${encodeURIComponent(loc)}&categories=bars&sort_by=distance`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${yelpApiKey}`,
+        'Accept': 'application/json'
+      }
+    }
+
+    var yelpReq = https.request(options, function (resp) {
+      var chunks = []
+      var data
+
+      resp.on('data', function (d) {
+        chunks.push(d)
+      })
+
+      resp.on('error', function (e) {
+        console.error(e.message || e.error || e)
+      })
+
+      resp.on('end', function () {
+        data = JSON.parse(Buffer.concat(chunks))
+        res.json(data.businesses)
+      })
+    })
+    yelpReq.end()
+  })
+}
--- 27-Nightlife-App/src/components/App-Main.vue
+++ 27-Nightlife-App/src/components/App-Main.vue
@@ -21,76 +21,7 @@ export default {
     return {
       date: new Date(),
       location: '',
-      bars: [
-        {
-          'rating': 4,
-          'price': '$',
-          'phone': '+14152520800',
-          'id': 'E8RJkjfdcwgtyoPMjQ_Olg',
-          'alias': 'four-barrel-coffee-san-francisco',
-          'is_closed': false,
-          'categories': [
-            {
-              'alias': 'coffee',
-              'title': 'Coffee & Tea'
-            }
-          ],
-          'review_count': 1738,
-          'name': 'Four Barrel Coffee',
-          'url': 'https://www.yelp.com/biz/four-barrel-coffee-san-francisco',
-          'coordinates': {
-            'latitude': 37.7670169511878,
-            'longitude': -122.42184275
-          },
-          'image_url': 'http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg',
-          'location': {
-            'city': 'San Francisco',
-            'country': 'US',
-            'address2': '',
-            'address3': '',
-            'state': 'CA',
-            'address1': '375 Valencia St',
-            'zip_code': '94103',
-            'display_address': ['375 Valencia St', 'San Francisco, CA 94103']
-          },
-          'distance': 1604.23,
-          'transactions': ['pickup', 'delivery']
-        },
-        {
-          'rating': 3.5,
-          'price': '$$',
-          'phone': '+14152520800',
-          'id': 'E8RJkjfdcwgtyoPMjQ_Olg',
-          'alias': 'four-barrel-coffee-san-francisco',
-          'is_closed': false,
-          'categories': [
-            {
-              'alias': 'coffee',
-              'title': 'Coffee & Tea'
-            }
-          ],
-          'review_count': 1738,
-          'name': 'Three Cup Coffee',
-          'url': 'https://www.yelp.com/biz/four-barrel-coffee-san-francisco',
-          'coordinates': {
-            'latitude': 37.7670169511878,
-            'longitude': -122.42184275
-          },
-          'image_url': 'http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg',
-          'location': {
-            'city': 'San Francisco',
-            'country': 'US',
-            'address2': '',
-            'address3': '',
-            'state': 'CA',
-            'address1': '375 Valencia St',
-            'zip_code': '94103',
-            'display_address': ['375 Valencia St', 'San Francisco, CA 94103']
-          },
-          'distance': 1604.23,
-          'transactions': ['pickup', 'delivery']
-        }
-      ]
+      bars: []
     }
   },
   computed: {
@@ -102,8 +33,20 @@ export default {
   },
   methods: {
     submit: function (location) {
-      this.location = location
-      console.log(this.location)
+      this.location = 'searching'
+      fetch(`/api?loc=${encodeURIComponent(location)}`)
+        .then(res => {
+          this.location = 'loading'
+          return res.json()
+        })
+        .catch(err => {
+          this.location = 'error'
+          console.error(err)
+        })
+        .then(bars => {
+          this.location = location
+          this.bars = bars
+        })
     }
   }
 }
